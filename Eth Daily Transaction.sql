4e2912a76b95b8e8c26e3c0173c0c53c8f83cac6
Charchit Kumawat <105235449+Charchitkumawat04@users.noreply.github.com>
2025-05-01 04:45:58 +0530

    Create Eth Daily Transaction.sql

---
+++ Eth Daily Transaction.sql
@@ -0,0 +1,10 @@
+SELECT
+  DATE(block_time) AS block_date,
+  COUNT(DISTINCT hash) AS daily_transactions
+FROM ethereum.transactions
+WHERE
+  block_time >= TRY_CAST('2024-11-01' AS TIMESTAMP)
+GROUP BY
+  1
+ORDER BY
+  block_date
